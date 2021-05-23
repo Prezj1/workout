5a6b47608c4f6333f6e22003f2d5a9fd40482801
Joy <joymakasi@hotmail.co.uk>
2021-05-23 10:37:10 +0100

    adding scripts

---
+++ build/precache-manifest.9bbd0aa7692adac312d1ee9449f04a1e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d01af9394876ef7677f41278e674593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0dceb5055cd810a7498",
+    "url": "/static/js/2.acf6bc4b.chunk.js"
+  },
+  {
+    "revision": "47b28fb389b074e86ef5",
+    "url": "/static/js/main.8dc30334.chunk.js"
+  },
+  {
+    "revision": "cf501e241f72b03df859",
+    "url": "/static/js/runtime-main.c8ce9901.js"
+  }
+]);
